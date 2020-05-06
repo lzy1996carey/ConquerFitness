1153b60168377b0ebb576e8a653af0067da787dc
lzy1996carey <877417049@qq.com>
2020-05-06 21:12:38 +0800

    改了首页和动作页，动作页用了组件库显示不了

--- pages/Train/Train.js
+++ pages/Train/Train.js
@@ -5,7 +5,49 @@ Page({
    * 页面的初始数据
    */
   data: {
+    option1: [
+      { text: '按部位', value: 0 },
+      { text: '按动作', value: 1 }
+    ],
+    value1: 0,
 
+    mainActiveIndex: 0,
+    activeId: null,
+    items:[
+      {
+        // 导航名称
+        text: '所有城市',
+        // 禁用选项
+        disabled: false,
+        // 该导航下所有的可选项
+        children: [
+          {
+            // 名称
+            text: '温州',
+            // id，作为匹配选中状态的标识
+            id: 1,
+            // 禁用选项
+            disabled: true
+          },
+          {
+            text: '杭州',
+            id: 2
+          }
+        ]
+      }
+    ]
+  },
+
+  onClickNav({ detail = {} }) {
+    this.setData({
+      mainActiveIndex: detail.index || 0
+    });
+  },
+
+  onClickItem({ detail = {} }) {
+    const activeId = this.data.activeId === detail.id ? null : detail.id;
+
+    this.setData({ activeId });
   },
 
   /**
@@ -74,5 +116,8 @@ Component({
         })
       }
     }
+  },
+  options: {
+    styleIsolation: 'shared'
   }
 })
